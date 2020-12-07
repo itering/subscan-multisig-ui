78a7f7d93133606e37b008d898404118ac620b9c
carlhong <acarlhong@gmail.com>
2020-12-07 16:58:23 +0800

    add jest unit test plugin

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-import clickoutside from './clickoutside';
 import clipboard from './clipboard';
 
-Vue.directive('clickoutside', clickoutside);
 Vue.directive('clipboard', clipboard);
